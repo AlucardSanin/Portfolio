09f599d366524143170de3757d646caa4b032a5b
AlucardSanin <alucardsanin@gmail.com>
2022-07-20 00:13:11 -0400

    Fixed ESLint Warnings

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-document.querySelector('.menu').addEventListener('click',display);
+function hide() {
+    document.querySelector('.mobile-menu').style.display= 'none';}
 
-function display(){
-    document.querySelector('.mobile-menu').style.display= "block";
-    document.getElementById('portf').addEventListener('click', hide);
-    document.getElementById('abo').addEventListener('click', hide);
-    document.getElementById('cont').addEventListener('click', hide);
-    document.getElementById('close').addEventListener('click', hide);
+function display() {
+  document.querySelector('.mobile-menu').style.display = 'block';
+  document.getElementById('portf').addEventListener('click', hide);
+  document.getElementById('abo').addEventListener('click', hide);
+  document.getElementById('cont').addEventListener('click', hide);
+  document.getElementById('close').addEventListener('click', hide);
 }
-
-function hide(){
-document.querySelector('.mobile-menu').style.display= "none";}
+  document.querySelector('.menu').addEventListener('click', display);
