bedd08c42a19ff1ef144ae4f7c9bfc655ccc7605
AlucardSanin <alucardsanin@gmail.com>
2022-07-20 00:03:54 -0400

    Fix ESLint Warnings

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ document.querySelector('.menu').addEventListener('click',display);
 
 function display(){
     document.querySelector('.mobile-menu').style.display= "block";
-    document.getElementById('portf').addEventListener('click',hide);
-    document.getElementById('abo').addEventListener('click',hide);
-    document.getElementById('cont').addEventListener('click',hide);
-    document.getElementById('close').addEventListener('click',hide);
+    document.getElementById('portf').addEventListener('click', hide);
+    document.getElementById('abo').addEventListener('click', hide);
+    document.getElementById('cont').addEventListener('click', hide);
+    document.getElementById('close').addEventListener('click', hide);
 }
 
 function hide(){
