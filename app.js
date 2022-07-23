20bbaa69cb4368d32e25e6d1070dbdf8508cc991
AlucardSanin <alucardsanin@gmail.com>
2022-07-23 11:33:58 -0400

    Validate Mail

--- app.js
+++ app.js
@@ -104,13 +104,14 @@ document.querySelector('.nav-bar').style.display = 'none';
 }
 
   const mail = document.getElementById('email');
-  var mailformat =/^[a-z\-0-9\.\*\#\$\!\~\%\^\&\-\+\?\|]+@+[a-z\-0-9]+(.com)$/;
+  var mailformat = /^[a-z\-0-9\.\*\#\$\!\~\%\^\&\-\+\?\|]+@+[a-z\-0-9]+(.com)$/;
   const form = document.getElementById('Submit');
 
   form.addEventListener('submit', function (event) {
   if (mail.value.match(mailformat)) {
     mail.setCustomValidity("Way to go!");
-    mail.reportValidity();    
+    mail.reportValidity(); 
+      
   } else {
     event.preventDefault();
     mail.setCustomValidity("Only accepts lowercase characters!");
